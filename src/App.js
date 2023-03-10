e4c1badef7c68d9bb3a34441f52ae49b7cd94ef3
Gamze <gamzeyasar35@icloud.com>
2023-03-10 15:59:48 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import Header from "./components/Header/header";
+import Main from "./components/Main/Main";
+import Navbar from "./components/Navbar/Navbar";
+import './Sass/App.scss';
+
 
 
 function App() {
   return (
     <div >
+      <Header />
+      <Navbar />
+      <Main />
 
     </div>
   );
